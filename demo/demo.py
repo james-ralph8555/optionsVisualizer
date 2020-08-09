e31e23da103746748f932b99394dcf99948fb8df
james-ralph8555 <53407847+james-ralph8555@users.noreply.github.com>
2020-08-09 19:02:55 -0400

    Add files via upload

--- demo/demo.py
+++ demo/demo.py
@@ -2,39 +2,52 @@ from pricing import option
 from scipy import io
 import numpy as np
 
-pb = option(otype='put', marketPrice=5.78, S0=334.54, K=325, expDay='2020-09-18')
-ps = option(otype='put', marketPrice=8.98, S0=334.54, K=335, expDay='2020-09-18')
-cs = option(otype='call', marketPrice=7.89, S0=334.54, K=335, expDay='2020-09-18')
-cb = option(otype='call', marketPrice=3.14, S0=334.54, K=345, expDay='2020-09-18')
+def sumprice(outs):
+    for i in range(len(outs)):
+        outs[i]['price']=np.array(outs[i]['price'])
+        if i == 0:
+            price=outs[0]['price']
+        else:
+            price+=outs[i]['price']
+    return price
+
+pb = option(otype='put', marketPrice=5.78, S0=334.54, K=325, expDay='2020-09-18', q=0.0171)
+ps = option(otype='put', marketPrice=8.98, S0=334.54, K=335, expDay='2020-09-18', q=0.0171)
+cs = option(otype='call', marketPrice=7.89, S0=334.54, K=335, expDay='2020-09-18', q=0.0171)
+cb = option(otype='call', marketPrice=3.14, S0=334.54, K=345, expDay='2020-09-18', q=0.0171)
 toSweep = {'S0' : (315, 355, 100), 'T' : (0.01, 0.11, 100)}
+toGrab = ('price')
 outs = []
-outs.append(pb.sweep(toSweep))
-outs.append(ps.sweep(toSweep))
-outs.append(cs.sweep(toSweep))
-outs.append(cb.sweep(toSweep))
+outs.append(pb.sweep(toSweep, toGrab))
+outs.append(ps.sweep(toSweep, toGrab))
+outs.append(cs.sweep(toSweep, toGrab))
+outs.append(cb.sweep(toSweep, toGrab))
 
-data = np.array(outs)
+price = sumprice(outs)
 
-io.savemat('data1.mat', {'S01': data[0,0], 'K1': data[:,1], 'vol1': data[:,2], 'r1': data[0,3], 'T1': data[0,4], 'price1': data[:,5]})
+io.savemat('data1.mat', {'S01': outs[0]['S0'], 'T1': outs[0]['T'], 'price1': price})
 
 toSweep = {'S0' : (315, 355, 100), 'vol' : (0.1, 0.3, 100)}
+toGrab = ('price')
 outs = []
-outs.append(pb.sweep(toSweep))
-outs.append(ps.sweep(toSweep))
-outs.append(cs.sweep(toSweep))
-outs.append(cb.sweep(toSweep))
+outs.append(pb.sweep(toSweep, toGrab))
+outs.append(ps.sweep(toSweep, toGrab))
+outs.append(cs.sweep(toSweep, toGrab))
+outs.append(cb.sweep(toSweep, toGrab))
 
-data = np.array(outs)
+price = sumprice(outs)
 
-io.savemat('data2.mat', {'S02': data[0,0], 'K2': data[:,1], 'vol2': data[2,2], 'r2': data[0,3], 'T2': data[0,4], 'price2': data[:,5]})
+io.savemat('data2.mat', {'S02': outs[0]['S0'], 'vol2': outs[2]['vol'], 'price2': price})
 
 toSweep = {'vol' : (0.1, 0.3, 100), 'T' : (0.01, 0.11, 100)}
+toGrab = ('price')
 outs = []
-outs.append(pb.sweep(toSweep))
-outs.append(ps.sweep(toSweep))
-outs.append(cs.sweep(toSweep))
-outs.append(cb.sweep(toSweep))
+outs.append(pb.sweep(toSweep, toGrab))
+outs.append(ps.sweep(toSweep, toGrab))
+outs.append(cs.sweep(toSweep, toGrab))
+outs.append(cb.sweep(toSweep, toGrab))
 
-data = np.array(outs)
+price = sumprice(outs)
 
-io.savemat('data3.mat', {'S03': data[0,0], 'K3': data[:,1], 'vol3': data[2,2], 'r3': data[0,3], 'T3': data[0,4], 'price3': data[:,5]})
+io.savemat('data3.mat', {'vol3': outs[2]['vol'], 'T3': outs[0]['T'], 'price3': price})
+
--- pricing.py
+++ pricing.py
@@ -3,20 +3,15 @@ import pandas_market_calendars as mcal
 import numpy as np
 from scipy import optimize
 from scipy import stats
-#import matplotlib.pyplot as plt
-#from mpl_toolkits.mplot3d import Axes3D
-#from matplotlib.colors import LinearSegmentedColormap
-#from parula import parula
-
-#parula_map = LinearSegmentedColormap.from_list('parula', parula())
 
 class option():
-    def __init__(self, otype, S0, K, marketPrice=None, T=None, expDay=None, vol=None, r=0.025):
+    def __init__(self, otype, S0, K, q=0, marketPrice=None, T=None, expDay=None, vol=None, r=0.025):
         self.S0=S0
         self.K=K
         self.r=r
         self.otype=otype.lower()
         self.daysInYear = 252
+        self.q=q
         
         if T is None and expDay is None:
             print('Please enter days to expiry or expiration day')
@@ -35,38 +30,28 @@ class option():
         elif vol is None:
             self.marketPrice=marketPrice
             self.vol=self.IV()
+            
+    def inpcheck(self, S0=None, K=None, vol=None, r=None, T=None, q=0):
+        if S0 is None: S0 = self.S0
+        if K is None: K = self.K
+        if vol is None: vol = self.vol
+        if r is None: r = self.r
+        if T is None: T = self.T
+        return S0, K, vol, r, T, q
     
-    def d1d2(self, S0=None, K=None, vol=None, r=None, T=None):
-        if S0 is None:
-            S0 = self.S0
-        if K is None:
-            K = self.K
-        if vol is None:
-            vol = self.vol
-        if r is None:
-            r = self.r
-        if T is None:
-            T = self.T
-        d1 = (np.log(S0/K)+(r + vol**2/2)*T)/(vol*np.sqrt(T))
+    def d1d2(self, S0=None, K=None, vol=None, r=None, T=None, q=0):
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1 = (np.log(S0/K)+(r - q + vol**2/2)*T)/(vol*np.sqrt(T))
         d2 = d1-vol*np.sqrt(T)
         return d1, d2
     
-    def price(self, S0=None, K=None, vol=None, r=None, T=None):
-        if S0 is None:
-            S0 = self.S0
-        if K is None:
-            K = self.K
-        if vol is None:
-            vol = self.vol
-        if r is None:
-            r = self.r
-        if T is None:
-            T = self.T
-        d1, d2 = self.d1d2(S0, K, vol, r, T)
+    def price(self, S0=None, K=None, vol=None, r=None, T=None, q=0):
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
         if self.otype == 'call':
-            return S0*stats.norm.cdf(d1)-K*np.exp(-r*T)*stats.norm.cdf(d2)
+            return S0*np.exp(-q*T)*stats.norm.cdf(d1)-K*np.exp(-r*T)*stats.norm.cdf(d2)
         elif self.otype == 'put':
-            return K*np.exp(-r*T)*stats.norm.cdf(-d2) - S0*stats.norm.cdf(-d1)
+            return K*np.exp(-r*T)*stats.norm.cdf(-d2) - S0*np.exp(-q*T)*stats.norm.cdf(-d1)
     
     def IV(self):
         c = lambda x: (self.price(vol=x)-self.marketPrice)**2
@@ -74,92 +59,114 @@ class option():
         res = optimize.minimize(c, x)
         return res.x[0]
     
-    def delta(self, S0=None, K=None, vol=None, r=None, T=None):
-        if S0 is None:
-            S0 = self.S0
-        if K is None:
-            K = self.K
-        if vol is None:
-            vol = self.vol
-        if r is None:
-            r = self.r
-        if T is None:
-            T = self.T
-        d1, d2 = self.d1d2(S0, K, vol, r, T)
-        if self.otype == 'call':
-            return stats.norm.cdf(d1)
-        elif self.otype == 'put':
-            return stats.norm.cdf(d1) - 1
+    #variables
+    #V - option price (referred to as price in code)
+    #S - underlying stock price (referred to as S0 in code)
+    #K - strike price
+    #vol - volatility
+    #r - risk free rate
+    #q - annual dividend yield
+    #T - time to expiration in years
     
-    def thetaperday(self, S0=None, K=None, vol=None, r=None, T=None):
-        return self.theta()/self.daysInYear
     
-    def theta(self, S0=None, K=None, vol=None, r=None, T=None):
-        if S0 is None:
-            S0 = self.S0
-        if K is None:
-            K = self.K
-        if vol is None:
-            vol = self.vol
-        if r is None:
-            r = self.r
-        if T is None:
-            T = self.T
-        d1, d2 = self.d1d2(S0, K, vol, r, T)
+    def delta(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #dV/dS
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
         if self.otype == 'call':
-            return (-(S0*stats.norm.pdf(d1)*vol)/(2*np.sqrt(T)) - r*K*np.exp(-r*T)*stats.norm.cdf(d2))
+            return np.exp(-q*T)*stats.norm.cdf(d1)
         elif self.otype == 'put':
-            return (-(S0*stats.norm.pdf(d1)*vol)/(2*np.sqrt(T)) + r*K*np.exp(-r*T)*stats.norm.cdf(-d2))
+            return np.exp(-q*T)*stats.norm.cdf(-d1)
         
-    def gamma(self, S0=None, K=None, vol=None, r=None, T=None):
-        if S0 is None:
-            S0 = self.S0
-        if K is None:
-            K = self.K
-        if vol is None:
-            vol = self.vol
-        if r is None:
-            r = self.r
-        if T is None:
-            T = self.T
-        d1, d2 = self.d1d2(S0, K, vol, r, T)
-        return (stats.norm.pdf(d1)/(S0*vol*np.sqrt(T)))
+    def vega(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #dV/dvol
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
+        return np.exp(-q*T)*S0*np.sqrt(T)*stats.norm.pdf(d1)
     
-    def vega(self, S0=None, K=None, vol=None, r=None, T=None):
-        if S0 is None:
-            S0 = self.S0
-        if K is None:
-            K = self.K
-        if vol is None:
-            vol = self.vol
-        if r is None:
-            r = self.r
-        if T is None:
-            T = self.T
-        d1, d2 = self.d1d2(S0, K, vol, r, T)
-        return S0*np.sqrt(T)*stats.norm.pdf(d1)
+    def theta(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #-dV/dT
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
+        if self.otype == 'call':
+            return -(np.exp(-q*T)*S0*stats.norm.pdf(d1)*vol)/(2*np.sqrt(T)) - r*K*np.exp(-r*T)*stats.norm.cdf(d2) + q*S0*np.exp(-q*T)*stats.norm.cdf(d1) 
+        elif self.otype == 'put':
+            return -(np.exp(-q*T)*S0*stats.norm.pdf(-d1)*vol)/(2*np.sqrt(T)) + r*K*np.exp(-r*T)*stats.norm.cdf(-d2) - q*S0*np.exp(-q*T)*stats.norm.cdf(-d1) 
     
-    def rho(self, S0=None, K=None, vol=None, r=None, T=None):
-        if S0 is None:
-            S0 = self.S0
-        if K is None:
-            K = self.K
-        if vol is None:
-            vol = self.vol
-        if r is None:
-            r = self.r
-        if T is None:
-            T = self.T
-        d1, d2 = self.d1d2(S0, K, vol, r, T)
+    def rho(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #dV.dr
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
         if self.otype == 'call':
             return K*T*np.exp(-r*T)*stats.norm.cdf(d2)
         elif self.otype == 'put':
             return -K*T*np.exp(-r*T)*stats.norm.cdf(-d2)
+        
+    def omega(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #aka lambda - leverage = dV/dS * S/V
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        return self.delta(S0, K, vol, r, T, q) * (S0/self.price(S0, K, vol, r, T, q))
+    
+    def gamma(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #d^2V/dS^2
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
+        return np.exp(-q*T)*(stats.norm.pdf(d1)/(S0*vol*np.sqrt(T)))
+    
+    def vanna(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #d^2V/dSdvol
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
+        return -np.exp(-q*T)*stats.norm.pdf(d1)*d2/vol
+    
+    def charm(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #-d^2V/dTdS
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
+        if self.otype == 'call':
+            return q*np.exp(-q*T)*stats.norm.cdf(d1) - np.exp(-q*T)*stats.norm.pdf(d1)*((2*(r-q)*T - d2*vol*np.sqrt(T))/(2*T*vol*np.sqrt(T)))
+        elif self.otype == 'put':
+            return -q*np.exp(-q*T)*stats.norm.cdf(-d1) - np.exp(-q*T)*stats.norm.pdf(d1)*((2*(r-q)*T - d2*vol*np.sqrt(T))/(2*T*vol*np.sqrt(T)))
+    
+    def vomma(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #d^2V/dvol^2
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
+        return S0*np.exp(-q*T)*stats.norm.pdf(d1)*np.sqrt(T)*((d1*d2)/vol)
+    
+    def veta(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #d^2V/dvoldT
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
+        return -S0*np.exp(-q*T)*stats.norm.pdf(d1)*np.sqrt(T)*(q+(((r-q)*d1)/(vol*np.sqrt(T)))-(1+d1*d2)/(2*T))
+    
+    def speed(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #d^3V/dS^3
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
+        return -np.exp(-q*T)*((stats.norm.pdf(d1))/(S0**2*vol*np.sqrt(T)))*(d1/(vol*np.sqrt(T))+1)
+    
+    def zomma(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #d^3V/dS^2dvol
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
+        return np.exp(-q*T)*((stats.norm.pdf(d1)*(d1*d2-1))/(S0*vol**2*np.sqrt(T)))
+    
+    def color(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #d^3V/dS^2dT
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
+        return -np.exp(-q*T)*(stats.norm.pdf(d1)/(2*S0*T*vol*np.sqrt(T)))
+    
+    def ultima(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #d^3V/dvol^3
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
+        return (-self.vega(S0, K, vol, r, T, q)/vol**2)*(d1*d2*(1-d1*d2)+d1**2+d2**2)
+    
+    def dualDelta(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #dV/dK
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
+        if self.otype == 'call':
+            return -np.exp(-r*T)*stats.norm.cdf(d2)
+        elif self.otype == 'put':
+            return -np.exp(-r*T)*stats.norm.cdf(-d2)
+    
+    def dualGamma(self, S0=None, K=None, vol=None, r=None, T=None, q=0): #d^2V/dK^2
+        S0, K, vol, r, T, q = self.inpcheck(S0, K, vol, r, T, q)
+        d1, d2 = self.d1d2(S0, K, vol, r, T, q)
+        return np.exp(-r*T)*(stats.norm.pdf(d2)/(K*vol*np.sqrt(T)))
     
     #toSweep dictionary with variables to sweep as key and value as (min, max, steps)
-    def sweep(self, toSweep):
-        inps = {'S0':self.S0, 'K':self.K, 'vol':self.vol, 'r':self.r, 'T':self.T}
-        vectors = dict([(k, np.linspace(toSweep[k][0], toSweep[k][1], toSweep[k][2])) for k in toSweep if k in ('S0', 'K', 'vol', 'r', 'T')])
+    def sweep(self, toSweep, toGrab):
+        inps = {'S0':self.S0, 'K':self.K, 'vol':self.vol, 'r':self.r, 'T':self.T, 'q':self.q}
+        vectors = dict([(k, np.linspace(toSweep[k][0], toSweep[k][1], toSweep[k][2])) for k in toSweep])
         scalars = dict([(k, inps[k]) for k in inps if k not in vectors])
         if len(toSweep) == 1:
             out = vectors.values[0]
@@ -169,11 +176,26 @@ class option():
             out = np.meshgrid(list(vectors.values())[0], list(vectors.values())[1], list(vectors.values())[2])
         grids = dict([(k, out[i]) for i, k in enumerate(vectors)])
         combined = {**grids, **scalars}
-            
-        price = self.price(combined['S0'], combined['K'], combined['vol'], combined['r'], combined['T'])
-        delta = self.delta(combined['S0'], combined['K'], combined['vol'], combined['r'], combined['T'])
-        theta = self.theta(combined['S0'], combined['K'], combined['vol'], combined['r'], combined['T'])
-        gamma = self.gamma(combined['S0'], combined['K'], combined['vol'], combined['r'], combined['T'])
-        vega = self.vega(combined['S0'], combined['K'], combined['vol'], combined['r'], combined['T'])
-        rho = self.rho(combined['S0'], combined['K'], combined['vol'], combined['r'], combined['T'])
-        return combined['S0'], combined['K'], combined['vol'], combined['r'], combined['T'], price, delta, theta, gamma, vega, rho 
+        
+        data = (combined['S0'], combined['K'], combined['vol'], combined['r'], combined['T'], combined['q'])
+        out = {}
+        
+        if 'price' in toGrab: out['price'] = self.price(*data)
+        if 'delta' in toGrab: out['delta'] = self.delta(*data)
+        if 'vega' in toGrab: out['vega'] = self.vega(*data)
+        if 'theta' in toGrab: out['theta'] = self.theta(*data)
+        if 'rho' in toGrab: out['rho'] = self.rho(*data)
+        if 'omega' in toGrab: out['omega'] = self.omega(*data)
+        if 'gamma' in toGrab: out['gamma'] = self.gamma(*data)
+        if 'vanna' in toGrab: out['vanna'] = self.vanna(*data)
+        if 'charm' in toGrab: out['charm'] = self.charm(*data)
+        if 'vomma' in toGrab: out['vomma'] = self.vomma(*data)
+        if 'veta' in toGrab: out['veta'] = self.veta(*data)
+        if 'speed' in toGrab: out['speed'] = self.speed(*data)
+        if 'zomma' in toGrab: out['zomma'] = self.zomma(*data)
+        if 'color' in toGrab: out['color'] = self.color(*data)
+        if 'ultima' in toGrab: out['ultima'] = self.ultima(*data)
+        if 'dualDelta' in toGrab: out['dualDelta'] = self.dualDelta(*data)
+        if 'dualGamma' in toGrab: out['dualGamma'] = self.dualGamma(*data)
+        
+        return {**combined, **out}
